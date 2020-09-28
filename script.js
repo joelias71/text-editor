47a2a429aaf4686d70f779e7a0c18c2a06fd36c8
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-28 13:36:33 -0500

    feat(text) Add bold, italic, and underline features

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ function updateText() {
     const text = document.getElementById('text-input').value
     document.getElementById('text-output').innerText = text
 }
+
+function makeBold(elem) {
+    elem.classList.toggle('active')
+    document.getElementById('text-output').classList.toggle('bold')
+}
+
+function makeItalic(elem) {
+    elem.classList.toggle('active')
+    document.getElementById('text-output').classList.toggle('italic')
+}
+
+function makeUnderline(elem) {
+    elem.classList.toggle('active')
+    //easy way use toggle
+    //document.getElementById('text-output').classList.toggle('underline')
+    const formattedText = document.getElementById('text-output')
+    if(formattedText.classList.contains('underline')) {
+        formattedText.classList.remove('underline')
+    } else {
+        formattedText.classList.add('underline')
+    }
+
+}
